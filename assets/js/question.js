54fb4f29afeb4c9bad3b6691a826135b07400400
ZahraCheraghian <155820127+zahra-cheraghian@users.noreply.github.com>
2024-04-27 10:40:02 +0330

    Add files via upload

---
+++ assets/js/question.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+        numb: 1,
+        question: "What is the coldest continent on Earth?",
+        answer: "Antarctica",
+        options: [
+            "Antarctica",
+            "Australia",
+            "South America",
+            "Africa"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Where is the Great Sphinx Statue located?",
+        answer: "Egypt",
+        options: [
+            "Syria",
+            "Grees",
+            "Egypt",
+            "Morocco"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is the capital of Japan?",
+        answer: "Tokyo",
+        options: [
+            "New Delhi",
+            "Seoul",
+            "Osaka",
+            "Tokyo"
+        ]
+    },
+    {
+        numb: 4,
+        question: "At less than 1 kilometer What is the Smallest country in the World?",
+        answer: "Vatican City",
+        options: [
+            "Croatia",
+            "Vatican City",
+            "Tuvalu",
+            "San Marino"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "eXtensible Markup Language",
+        options: [
+            "eXtensible Markup Language",
+            "eXecutable Multiple Language",
+            "eXTra Multi-Program Language",
+            "eXamine Multiple Language"
+        ]
+    },
+    
+];
